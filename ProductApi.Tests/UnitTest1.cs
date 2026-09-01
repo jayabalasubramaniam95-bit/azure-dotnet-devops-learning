3cd1632f5ee22965f162356e1e9f5b2f60e357b9
Jayashree Balasubramaniam <jayabalasubramaniam95@gmail.com>
2026-09-01 21:08:36 +1200

    Update CI to run unit tests

--- ProductApi.Tests/UnitTest1.cs
+++ ProductApi.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@ public class ProductTests
     [Fact]
     public void Product_Id_Should_Be_Positive()
     {
-        var productId = 0
+        var productId = 0;
 
         Assert.True(productId > 0);
     }
