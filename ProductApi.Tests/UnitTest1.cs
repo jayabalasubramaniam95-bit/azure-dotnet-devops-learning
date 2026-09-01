b0bb9440983964bea9a6ea361631329a627186ea
Jayashree Balasubramaniam <jayabalasubramaniam95@gmail.com>
2026-09-01 19:16:44 +1200

    Add Product API unit tests

---
+++ ProductApi.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductApi.Tests;
+
+public class ProductTests
+{
+    [Fact]
+    public void Product_Id_Should_Be_Positive()
+    {
+        var productId = 2;
+
+        Assert.True(productId > 0);
+    }
+}
